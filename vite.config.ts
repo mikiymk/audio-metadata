aa5fefadeef108795c3d9d5c2f9ddf71f1e0a8f1
mikiymk <mikiymk001@gmail.com>
2022-10-17 21:21:21 +0900

    minify pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      name: "AudioMetadata",
-      formats: ["es", "cjs", "umd", "iife"],
+export default defineConfig(({ mode }) => {
+  if (mode === "pages")
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            id3v1: resolve(__dirname, "pages", "id3v1.html"),
+            id3v2: resolve(__dirname, "pages", "id3v2.html"),
+            ogg: resolve(__dirname, "pages", "ogg.html"),
+            flac: resolve(__dirname, "pages", "flac.html"),
+            wma: resolve(__dirname, "pages", "wma.html"),
+          },
+        },
+
+        outDir: "minpages",
+      },
+    };
+
+  return {
+    build: {
+      lib: {
+        entry: "src/index.ts",
+        name: "AudioMetadata",
+        formats: ["es", "cjs", "umd", "iife"],
+      },
+
+      outDir: "min",
     },
-    outDir: "min"
-  },
+  };
 });
