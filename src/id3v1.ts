d5ad6b64a19ec791bc2b012f2a45f130a4021ac4
mikiymk <mikiymk001@gmail.com>
2022-10-19 23:42:14 +0900

    id3v1 uses reader view

--- src/id3v1.ts
+++ src/id3v1.ts
@@ -1,9 +1,10 @@
-import { createView, readAscii, trimNull } from "./utils";
+import { createReaderView, EncAscii, getString, getUint, moveAbs, moveRel, ReaderView } from "./reader";
+import { trimNull } from "./utils";
 
-const checkMagicId3v1 = (view: DataView): boolean => {
-  const id3Magic = readAscii(view, view.byteLength - 128, 3);
+const checkMagicId3v1 = (view: ReaderView): boolean => {
+  moveAbs(view, -128);
   //"TAG"
-  return id3Magic === "TAG";
+  return getString(view, 3, EncAscii) === "TAG";
 };
 
 export type ID3v1 = {
@@ -23,24 +24,27 @@ export type ID3v1 = {
  */
 export const id3v1 = (buffer: Uint8Array | ArrayBufferLike): ID3v1 | undefined => {
   //read last 128 bytes
-  const view = createView(buffer);
+  const view = createReaderView(buffer);
   try {
+    //"TAG"
     if (!checkMagicId3v1(view)) {
       return undefined;
     }
 
-    const offset = view.byteLength - 128,
-      //next byte is the track
-      hasTrack = view.getUint8(offset + 125) === 0,
-      title = trimNull(readAscii(view, offset + 3, 30)),
-      artist = trimNull(readAscii(view, offset + 33, 30)),
-      album = trimNull(readAscii(view, offset + 63, 30)),
-      year = trimNull(readAscii(view, offset + 93, 4)),
-      comment = trimNull(readAscii(view, offset + 97, hasTrack ? 28 : 30)),
-      track = hasTrack ? view.getUint8(offset + 126) : undefined,
-      genre = view.getUint8(offset + 127);
+    //next byte is the track
+    moveRel(view, 122);
+    const hasTrack = getUint(view, 1) === 0;
+    moveRel(view, -123);
 
-    return { title, artist, album, year, comment, track, genre };
+    return {
+      title: trimNull(getString(view, 30, EncAscii)),
+      artist: trimNull(getString(view, 30, EncAscii)),
+      album: trimNull(getString(view, 30, EncAscii)),
+      year: trimNull(getString(view, 4, EncAscii)),
+      comment: trimNull(getString(view, hasTrack ? 28 : 30, EncAscii)),
+      track: hasTrack ? getUint(view, 2) : undefined,
+      genre: getUint(view, 1),
+    };
   } catch {
     return undefined;
   }
