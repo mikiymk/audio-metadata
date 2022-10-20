fc19bb6b4daf32b9d8ff6e48de5df478ab8cad9b
mikiymk <mikiymk001@gmail.com>
2022-10-20 23:21:28 +0900

    mp4 uses reader view

--- src/mp4.ts
+++ src/mp4.ts
@@ -5,42 +5,43 @@
  * https://github.com/sannies/mp4parser/
  */
 
-import { createView, readAscii, readBytes, readUtf16be, readUtf8 } from "./utils";
+import { createReaderView, EncAscii, EncUtf16be, EncUtf8, getBytes, getString, getUint, moveRel, peek, ReaderView, restLength } from "./reader";
 
 // moov trak mdia udta
 // + meta
 //   + ilst
+//     + data
 
 interface Atom {
   size: number;
   type: string;
-  data: DataView;
+  data: ReaderView;
 }
 
-const parseAtom = (view: DataView, offset: number): Atom | undefined => {
+const parseAtom = (view: ReaderView): Atom | undefined => {
   try {
     // if size == 1, atom has extended size
     // if size == 0, atom is last atom of container and size is reach to end
-    const rawSize = view.getUint32(offset);
-    const size = rawSize === 1 ? Number(view.getBigUint64(offset + 8)) : rawSize || view.byteLength - offset;
+    const rawSize = getUint(view, 4);
+    const type = getString(view, 4, EncAscii).toLowerCase();
+    const size = rawSize === 1 ? getUint(view, 8) : rawSize || restLength(view);
     const headerSize = rawSize === 1 ? 16 : 8;
 
     return {
       size,
-      type: readAscii(view, offset + 4, 4).toLowerCase(),
-      data: createView(readBytes(view, offset + headerSize, size - headerSize)),
+      type,
+      data: createReaderView(getBytes(view, size - headerSize)),
     };
   } catch {
     return undefined;
   }
 };
 
-const parseAtomList = function* (view: DataView): Generator<Atom> {
+const parseAtomList = function* (view: ReaderView): Generator<Atom> {
   let atom;
-  let offset = 0;
   let safetyCount = 0;
 
-  while ((atom = parseAtom(view, (offset += atom?.size ?? 0)))) {
+  while ((atom = parseAtom(view))) {
     if (safetyCount++ > 100) break;
 
     yield atom;
@@ -61,51 +62,56 @@ const TypeMap: Record<string, string> = {
   gnre: "genre",
 };
 
-const parseItem = (view: DataView): string | undefined => {
+const parseItem = (view: ReaderView): string | undefined => {
   const data = [...parseAtomList(view)].find((value) => value.type === "data");
 
   if (data) {
-    switch (data.data.getUint32(0)) {
+    const itemData = data.data;
+    const type = getUint(itemData, 4);
+    moveRel(itemData, 4);
+    switch (type) {
       case 1:
       case 4:
-        return readUtf8(data.data, 8, data.data.byteLength - 8);
+        return getString(itemData, restLength(itemData), EncUtf8);
       case 2:
       case 5:
-        return readUtf16be(data.data, 8, data.data.byteLength - 8);
+        return getString(itemData, restLength(itemData), EncUtf16be);
     }
   }
 };
-const parseItemList = (view: DataView): Record<string, string> => {
+
+const parseItemList = (view: ReaderView): Record<string, string> => {
   const metadatas: Record<string, string> = {};
 
-  for (const atom of parseAtomList(view)) {
+  for (const { data, type } of parseAtomList(view)) {
+    const item = parseItem(data);
     Object.assign(metadatas, {
-      [atom.type]: parseItem(atom.data),
-      [TypeMap[atom.type] || atom.type]: parseItem(atom.data),
+      [type]: item,
+      [TypeMap[type] || type]: item,
     });
   }
 
   return metadatas;
 };
 
-const parseAtoms = (view: DataView): Record<string, string> => {
+const parseAtoms = (view: ReaderView): Record<string, string> => {
   const metadatas: Record<string, string> = {};
 
-  for (const atom of parseAtomList(view)) {
-    switch (atom.type) {
+  for (const { type, data } of parseAtomList(view)) {
+    switch (type) {
       case "moov":
       case "trak":
       case "mdia":
       case "udta":
-        Object.assign(metadatas, parseAtoms(atom.data));
+        Object.assign(metadatas, parseAtoms(data));
         break;
 
       case "meta":
-        Object.assign(metadatas, parseAtoms(metaBoxShift(atom.data)));
+        Object.assign(metadatas, parseAtoms(metaBoxShift(data)));
         break;
 
       case "ilst":
-        Object.assign(metadatas, parseItemList(atom.data));
+        Object.assign(metadatas, parseItemList(data));
         break;
 
       default:
@@ -115,14 +121,15 @@ const parseAtoms = (view: DataView): Record<string, string> => {
   return metadatas;
 };
 
-const metaBoxShift = (view: DataView): DataView => {
-  const type = readAscii(view, 4, 4);
-  const handler = readAscii(view, 16, 4);
+const metaBoxShift = (view: ReaderView): ReaderView => {
+  const type = peek(getString, 4)(view, 4, EncAscii);
+  const handler = peek(getString, 16)(view, 4, EncAscii);
 
   if (type === "hdlr" && handler === "mdta") {
     return view;
   }
-  return createView(readBytes(view, 4, view.byteLength - 4));
+  moveRel(view, 4);
+  return view;
 };
 
 /**
@@ -131,7 +138,7 @@ const metaBoxShift = (view: DataView): DataView => {
  * @returns mp4 meta object on success, undefined on failure
  */
 export const mp4 = (buffer: Uint8Array | ArrayBufferLike): Record<string, string> | undefined => {
-  const view = createView(buffer);
+  const view = createReaderView(buffer);
 
   const metadata = parseAtoms(view);
 
