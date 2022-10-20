39c9716f3829e5ad08bd3760d52743141b3f0124
mikiymk <mikiymk001@gmail.com>
2022-10-21 00:17:04 +0900

    minify source

--- min/audio-metadata.mjs
+++ min/audio-metadata.mjs
@@ -1,59 +1,96 @@
-const p = (e) => e.replace(/\0+$/, ""), E = (e, t) => {
-  const n = e.indexOf(t);
-  return [e.substring(0, n), e.substring(n + 1)];
-}, l = (e) => {
-  if (e instanceof Uint8Array && (e = e.buffer.slice(0)), !(e instanceof ArrayBuffer))
-    throw new Error("Expected instance of Buffer or ArrayBuffer");
-  return new DataView(e);
-}, b = (e, t, n) => new Uint8Array(e.buffer).slice(t, t + n), h = (e) => (t, n, r) => new TextDecoder(e).decode(t.buffer.slice(n, n + r)), u = h("ascii"), L = h(), B = h("utf-16be"), m = h("utf-16le"), T = (e, t) => {
-  if (e.byteLength < t + 27)
+const y = (t) => {
+  let e;
+  if (ArrayBuffer.isView(t))
+    e = new DataView(t.buffer, t.byteOffset, t.byteLength);
+  else if (t instanceof ArrayBuffer || t instanceof SharedArrayBuffer)
+    e = new DataView(t);
+  else
+    throw new Error("Expected instance of TypedArray, DataView or ArrayBuffer");
+  return {
+    view: e,
+    position: 0
+  };
+}, u = (t, e) => {
+  const n = t.position, o = t.view;
+  return t.position += e, { view: o, position: n };
+}, V = (t, e) => {
+  const n = t.position, o = t.view;
+  return t.position = e < 0 ? o.byteLength + e : e, { view: o, position: n };
+}, k = (t) => t.view.byteLength - t.position, s = (t, e, n = !1) => {
+  const { view: o, position: r } = u(t, e);
+  return Number(
+    o[{
+      1: "getUint8",
+      2: "getUint16",
+      4: "getUint32",
+      8: "getBigUint64"
+    }[e]](r, n)
+  );
+}, B = (t, e) => {
+  const { view: n, position: o } = u(t, e);
+  return n.buffer.slice(o, o + e);
+}, l = "ascii", C = "utf-8", L = "utf-16be", p = "utf-16le", a = (t, e, n = C) => new TextDecoder(n).decode(B(t, e)), S = (t, e) => new Uint8Array(B(t, e)), T = (t, e) => y(B(t, e)), E = (t, e = 0) => (...n) => {
+  const [{ view: o, position: r }, ...c] = n;
+  return t({ view: o, position: r + e }, ...c);
+}, d = (t) => t.replace(/\0+$/, ""), G = (t, e) => {
+  const n = t.indexOf(e);
+  return [t.substring(0, n), t.substring(n + 1)];
+}, z = (t) => {
+  if (k(t) < 27)
     return;
-  const n = e.getUint8(t + 26), r = b(e, t + 27, n), c = 27 + n;
-  if (!r.length)
+  const e = (u(t, 26), s(t, 1)), n = S(t, e);
+  if (!n.length)
     return;
-  const a = c + r.reduce((o, i) => o + i), s = c + 7;
-  return { pageSize: a, packet: l(b(e, t + s, a - s)) };
-}, S = (e) => {
+  const o = n.reduce((r, c) => r + c);
+  return u(t, 7), { pageSize: o + 27 + e, packet: T(t, o - 7) };
+}, I = (t) => {
   try {
-    const t = e.getUint32(0, !0), n = e.getUint32(4 + t, !0), r = {}, c = { tracknumber: "track" };
-    for (let a = 0, s = 8 + t; a < n; a++) {
-      const o = e.getUint32(s, !0), i = L(e, s + 4, o), [g, d] = E(i, "="), U = g.toLowerCase();
-      r[c[U] || U] = r[U] = p(d), s += 4 + o;
+    const e = s(t, 4, !0), n = (u(t, e), s(t, 4, !0)), o = {}, r = { tracknumber: "track" };
+    for (let c = 0; c < n; c++) {
+      const m = s(t, 4, !0), i = a(t, m), [f, g] = G(i, "="), h = f.toLowerCase();
+      o[r[h] || h] = o[h] = d(g);
     }
-    return r;
+    return o;
   } catch {
     return;
   }
-}, V = (e) => {
-  const t = l(e), n = T(t, 0);
-  if (!n)
+}, Y = (t) => {
+  const e = y(t);
+  if (!z(e))
     return;
-  const r = T(t, n.pageSize);
-  if (r)
-    return S(r.packet);
-}, O = (e) => u(e, e.byteLength - 128, 3) === "TAG", W = (e) => {
-  const t = l(e);
+  const o = z(e);
+  if (o)
+    return I(o.packet);
+}, H = (t) => (V(t, -128), a(t, 3, l) === "TAG"), Z = (t) => {
+  const e = y(t);
   try {
-    if (!O(t))
+    if (!H(e))
       return;
-    const n = t.byteLength - 128, r = t.getUint8(n + 125) === 0, c = p(u(t, n + 3, 30)), a = p(u(t, n + 33, 30)), s = p(u(t, n + 63, 30)), o = p(u(t, n + 93, 4)), i = p(u(t, n + 97, r ? 28 : 30)), g = r ? t.getUint8(n + 126) : void 0, d = t.getUint8(n + 127);
-    return { title: c, artist: a, album: s, year: o, comment: i, track: g, genre: d };
+    const n = E(s, 122)(e, 1) === 0;
+    return {
+      title: d(a(e, 30, l)),
+      artist: d(a(e, 30, l)),
+      album: d(a(e, 30, l)),
+      year: d(a(e, 4, l)),
+      comment: d(a(e, n ? 28 : 30, l)),
+      track: n ? s(e, 2) : void 0,
+      genre: s(e, 1)
+    };
   } catch {
     return;
   }
-}, y = (e, t) => b(e, t, 4).reduce((n, r) => n << 7 | r & 268435455, 0), z = (e) => {
-  switch (e) {
-    case 1:
-      return (t, n, r) => t.getUint16(n) === 65279 ? B(t, n, r) : h("utf16le")(t, n, r);
-    case 2:
-      return h("utf16be");
-    case 3:
-      return L;
-    case 0:
-    default:
-      return u;
-  }
-}, F = {
+}, x = (t) => S(t, 4).reduce((e, n) => e << 7 | n & 127, 0), O = (t, e) => {
+  var n;
+  return a(
+    t,
+    e - 1,
+    (n = {
+      0: l,
+      1: E(s)(t, 2) === 65279 ? L : p,
+      2: L
+    }[s(t, 1)]) != null ? n : C
+  );
+}, R = {
   TALB: "album",
   TCOM: "composer",
   TIT1: "title",
@@ -63,139 +100,138 @@ const p = (e) => e.replace(/\0+$/, ""), E = (e, t) => {
   TSSE: "encoder",
   TDRC: "year",
   TCON: "genre"
-}, j = (e, t) => {
+}, N = (t) => {
   try {
-    const n = u(e, t, 4), r = 10 + y(e, t + 4), c = n[0] === "T" ? p(z(e.getUint8(t + 9))(e, t + 11, r - 11)) : void 0;
-    return { id: n, size: r, content: c };
+    const e = a(t, 4, l), n = x(t);
+    u(t, 2);
+    const o = e[0] === "T" ? d(O(t, n)) : (u(t, n), void 0);
+    return { id: e, content: o };
   } catch {
     return;
   }
-}, G = (e) => {
-  const t = l(e);
-  if (u(t, 0, 3) !== "ID3")
+}, _ = (t) => {
+  const e = y(t);
+  if (a(e, 3, l) !== "ID3")
     return;
-  const n = t.getUint8(5), r = y(t, 6), c = !!(n & 128), a = {}, s = c ? y(t, 10) : 0;
-  for (let o = 10 + s; o < 10 + s + r; ) {
-    const i = j(t, o);
+  const n = (u(e, 2), s(e, 1)), o = x(e), r = !!(n & 128), c = {}, m = r ? x(e) : 0;
+  for (u(e, m); e.position < 10 + m + o; ) {
+    const i = N(e);
     if (!i)
       break;
-    if (o += i.size, !i.content)
+    if (!i.content)
       continue;
-    const g = F[i.id] || i.id;
-    if (g === "TXXX") {
-      const [d, U] = E(i.content, "\0");
-      a[d] = U;
+    const f = R[i.id] || i.id;
+    if (f === "TXXX") {
+      const [g, h] = G(i.content, "\0");
+      c[g] = h;
     } else
-      a[g] = a[i.id] = i.content;
-  }
-  return a;
-}, $ = (e) => u(e, 0, 4) === "fLaC", q = (e) => {
-  const t = l(e);
-  if (!$(t))
-    return;
-  let n = 4, r = !1, c = {};
-  for (; !r; ) {
-    const a = t.getUint8(n), s = t.getUint16(n + 1) * 2 ** 16 + t.getUint8(n + 3);
-    r = a > 127, (a & 127) == 4 && (c = Object.assign(c, S(l(b(t, n + 4, s))))), n += 4 + s;
+      c[f] = c[i.id] = i.content;
   }
   return c;
-}, A = (e, t) => {
-  let n = "";
-  for (let r = 0; r < 2; r++)
-    n += e.getBigUint64(t + r * 8).toString(16).padStart(16, "0").toUpperCase();
-  return n;
-}, M = (e, t) => {
-  const n = e.getUint16(t, !0), r = e.getUint16(t + 2, !0), c = e.getUint16(t + 4, !0), a = e.getUint16(t + 6, !0), s = e.getUint16(t + 8, !0);
-  return {
-    title: m(e, t += 10, n).replace(/\0$/, ""),
-    artist: m(e, t += n, r).replace(/\0$/, ""),
-    copyright: m(e, t += r, c).replace(/\0$/, ""),
-    comment: m(e, t += c, a).replace(/\0$/, ""),
-    rating: m(e, t += a, s).replace(/\0$/, "")
-  };
-}, D = {
-  "wm/albumtitle": "album"
-}, I = (e, t) => {
-  const n = {}, r = e.getUint16(t, !0);
-  t += 2;
-  for (let c = 0; c < r; c++) {
-    const a = e.getUint16(t, !0), s = m(e, t += 2, a).replace(/\0$/, "").toLowerCase(), o = s.startsWith("wm/") ? s.slice(3) : s, i = D[s] || s, g = e.getUint16(t += a, !0), d = e.getUint16(t += 2, !0);
-    switch (g) {
-      case 0:
-        n[o] = n[i] = m(e, t += 2, d).replace(/\0$/, "");
-        break;
-    }
-    t += d;
+}, P = (t) => {
+  const e = y(t);
+  if (a(e, 4, l) !== "fLaC")
+    return;
+  let n = !1, o = {};
+  for (; !n; ) {
+    const r = s(e, 1), c = s(e, 2) * 2 ** 8 + s(e, 1);
+    n = r > 127, (r & 127) == 4 ? o = { ...o, ...I(T(e, c)) } : u(e, c);
   }
-  return n;
-}, H = (e, t) => {
-  const n = e.getUint32(t + 18), r = {};
-  for (t += 22; t < n; ) {
-    switch (A(e, t)) {
-      case "EACBF8C5AF5B48778467AA8C44FA4CCA":
-      case "941C23449894D149A1411D134E457054":
-        Object.assign(r, N(e, t + 24));
-        break;
-    }
-    t += Number(e.getBigUint64(t + 16, !0));
-  }
-  return r;
-}, N = (e, t) => {
-  const n = {}, r = e.getUint16(t, !0);
-  t += 2;
-  for (let c = 0; c < r; c++) {
-    t += 4;
-    const a = e.getUint16(t, !0), s = e.getUint16(t += 2, !0), o = e.getUint16(t += 2, !0), i = m(e, t += 2, a).replace(/\0$/, "").toLowerCase(), g = i.startsWith("wm/") ? i.slice(3) : i, d = D[i] || i;
-    switch (s) {
-      case 0:
-        n[g] = n[d] = m(e, t += a, o).replace(/\0$/, "");
-        break;
-    }
-    t += o;
-  }
-  return n;
-}, J = (e) => {
-  const t = l(e);
-  if (A(t, 0) === "3026B2758E66CF11A6D900AA0062CE6C")
-    try {
-      const n = t.getBigUint64(16, !0), r = {};
-      let c = 30;
-      for (; c < n; ) {
-        switch (A(t, c)) {
-          case "3326B2758E66CF11A6D900AA0062CE6C":
-            Object.assign(r, M(t, c + 24));
-            break;
-          case "40A4D0D207E3D21197F000A0C95EA850":
-            Object.assign(r, I(t, c + 24));
-            break;
-          case "B503BF5F2EA9CF118EE300C00C205365":
-            Object.assign(r, H(t, c + 24));
-            break;
-          default:
-        }
-        c += Number(t.getBigUint64(c + 16, !0));
-      }
-      return r;
-    } catch {
-      return;
-    }
-}, P = (e, t) => {
+  return o;
+}, X = (t) => "G" + S(t, 16).reduce((e, n) => e + n.toString(16).padStart(2, "0").toUpperCase(), ""), D = (t) => {
   try {
-    const n = e.getUint32(t), r = n === 1 ? Number(e.getBigUint64(t + 8)) : n || e.byteLength - t, c = n === 1 ? 16 : 8;
+    const e = X(t), n = s(t, 8, !0), o = T(t, n - 24);
+    return { guid: e, size: n, data: o };
+  } catch {
+    return;
+  }
+}, j = (t) => {
+  const e = s(t, 2, !0), n = s(t, 2, !0), o = s(t, 2, !0), r = s(t, 2, !0), c = s(t, 2, !0);
+  return {
+    title: d(a(t, e, p)),
+    artist: d(a(t, n, p)),
+    copyright: d(a(t, o, p)),
+    comment: d(a(t, r, p)),
+    rating: d(a(t, c, p))
+  };
+}, M = {
+  "wm/albumtitle": "album"
+}, K = (t) => {
+  const e = {}, n = s(t, 2, !0);
+  for (let o = 0; o < n; o++) {
+    const r = s(t, 2, !0), c = d(a(t, r, p)).toLowerCase(), m = c.startsWith("wm/") ? c.slice(3) : c, i = M[c] || c, f = s(t, 2, !0), g = s(t, 2, !0);
+    switch (f) {
+      case 0:
+        e[m] = e[i] = d(a(t, g, p));
+        break;
+      default:
+        u(t, g);
+    }
+  }
+  return e;
+}, W = (t) => {
+  var o;
+  u(t, 22);
+  let e = {}, n;
+  for (; n = D(t); ) {
+    const r = {
+      GEACBF8C5AF5B48778467AA8C44FA4CCA: F,
+      G941C23449894D149A1411D134E457054: F
+    };
+    e = { ...e, ...(o = r[n.guid]) == null ? void 0 : o.call(r, n.data) };
+  }
+  return e;
+}, F = (t) => {
+  const e = {}, n = s(t, 2, !0);
+  for (let o = 0; o < n; o++) {
+    u(t, 4);
+    const r = s(t, 2, !0), c = s(t, 2, !0), m = s(t, 2, !0), i = d(a(t, r, p)).toLowerCase(), f = i.startsWith("wm/") ? i.slice(3) : i, g = M[i] || i;
+    switch (c) {
+      case 0:
+        e[f] = e[g] = d(a(t, m, p));
+        break;
+      default:
+        u(t, m);
+    }
+  }
+  return e;
+}, w = (t) => {
+  var n;
+  const e = y(t);
+  try {
+    const o = D(e);
+    if (!o || o.guid !== "G3026B2758E66CF11A6D900AA0062CE6C")
+      return;
+    u(o.data, 6);
+    let r, c = {};
+    for (; r = D(o.data); ) {
+      const m = {
+        G3326B2758E66CF11A6D900AA0062CE6C: j,
+        G40A4D0D207E3D21197F000A0C95EA850: K,
+        GB503BF5F2EA9CF118EE300C00C205365: W
+      };
+      c = { ...c, ...(n = m[r.guid]) == null ? void 0 : n.call(m, r.data) };
+    }
+    return c;
+  } catch {
+    return;
+  }
+}, v = (t) => {
+  try {
+    const e = s(t, 4), n = a(t, 4, l).toLowerCase(), o = e === 1 ? s(t, 8) : e || k(t);
     return {
-      size: r,
-      type: u(e, t + 4, 4).toLowerCase(),
-      data: l(b(e, t + c, r - c))
+      size: o,
+      type: n,
+      data: T(t, o - (e === 1 ? 16 : 8))
     };
   } catch {
     return;
   }
-}, k = function* (e) {
-  var c;
-  let t, n = 0, r = 0;
-  for (; (t = P(e, n += (c = t == null ? void 0 : t.size) != null ? c : 0)) && !(r++ > 100); )
-    yield t;
-}, X = {
+}, U = function* (t) {
+  let e, n = 0;
+  for (; (e = v(t)) && !(n++ > 100); )
+    yield e;
+}, $ = {
   "\xA9alb": "album",
   "\xA9wrt": "composer",
   "\xA9nam": "title",
@@ -207,55 +243,51 @@ const p = (e) => e.replace(/\0+$/, ""), E = (e, t) => {
   "\xA9day": "year",
   "\xA9gen": "genre",
   gnre: "genre"
-}, x = (e) => {
-  const t = [...k(e)].find((n) => n.type === "data");
-  if (t)
-    switch (t.data.getUint32(0)) {
-      case 1:
-      case 4:
-        return L(t.data, 8, t.data.byteLength - 8);
-      case 2:
-      case 5:
-        return B(t.data, 8, t.data.byteLength - 8);
-    }
-}, K = (e) => {
-  const t = {};
-  for (const n of k(e))
-    Object.assign(t, {
-      [n.type]: x(n.data),
-      [X[n.type] || n.type]: x(n.data)
-    });
-  return t;
-}, C = (e) => {
-  const t = {};
-  for (const n of k(e))
-    switch (n.type) {
-      case "moov":
-      case "trak":
-      case "mdia":
-      case "udta":
-        Object.assign(t, C(n.data));
-        break;
-      case "meta":
-        Object.assign(t, C(R(n.data)));
-        break;
-      case "ilst":
-        Object.assign(t, K(n.data));
-        break;
-    }
-  return t;
-}, R = (e) => {
-  const t = u(e, 4, 4), n = u(e, 16, 4);
-  return t === "hdlr" && n === "mdta" ? e : l(b(e, 4, e.byteLength - 4));
-}, Q = (e) => {
-  const t = l(e), n = C(t);
+}, q = (t) => {
+  const e = [...U(t)].find((n) => n.type === "data");
+  if (e) {
+    const n = e.data, o = s(n, 4);
+    u(n, 4);
+    const r = {
+      1: C,
+      2: L,
+      4: C,
+      5: L
+    }[o];
+    return r && a(n, k(n), r);
+  }
+}, J = (t) => {
+  let e = {};
+  for (const { data: n, type: o } of U(t)) {
+    const r = q(n);
+    r && (e = { ...e, [o]: r, [$[o] || o]: r });
+  }
+  return e;
+}, A = (t) => {
+  var n;
+  let e = {};
+  for (const { type: o, data: r } of U(t))
+    e = { ...e, ...(n = b[o]) == null ? void 0 : n.call(b, r) };
+  return e;
+}, b = {
+  moov: A,
+  trak: A,
+  mdia: A,
+  udta: A,
+  meta: (t) => A(Q(t)),
+  ilst: J
+}, Q = (t) => {
+  const e = E(a, 4)(t, 4, l), n = E(a, 16)(t, 4, l);
+  return e === "hdlr" && n === "mdta" || u(t, 4), t;
+}, tt = (t) => {
+  const e = y(t), n = A(e);
   return Object.keys(n).length === 0 ? void 0 : n;
 };
 export {
-  q as flac,
-  W as id3v1,
-  G as id3v2,
-  Q as mp4,
-  V as ogg,
-  J as wma
+  P as flac,
+  Z as id3v1,
+  _ as id3v2,
+  tt as mp4,
+  Y as ogg,
+  w as wma
 };
